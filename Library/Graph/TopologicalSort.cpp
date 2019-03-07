7549064e77989075deafc799d0c6deb6ebf7a9b9
MonaAhmed810 <monaahmed3040@outlook.com>
2019-03-07 14:53:16 +0200

    add topologicalSort

---
+++ Library/Graph/TopologicalSort.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 100005;
+vector<int> adj[N];
+bool vis[N];
+stack<int> topoStack;
+
+void topologicalSort(int i) {
+    vis[i] = true;
+    for (int v : adj[i])
+        if (!vis[v])
+            topologicalSort(v);
+    topoStack.push(i);
+}
